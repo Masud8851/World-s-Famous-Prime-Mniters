ffdb6d43d5ccb0ff44e211e6707c67cf177623b2
Masud8851 <masud1971.cse@gmail.com>
2021-09-26 01:05:43 +0600

    Assignment 8

--- src/Components/PrimeMinisters/PrimeMinisters.js
+++ src/Components/PrimeMinisters/PrimeMinisters.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const PrimeMinisters = () => {
+
+    const [primeMinisters, setPrimeMinisters] = useState([]);
+    useEffect(()=>{
+        fetch('./minister.JSON')
+            .then(res=>res.json())
+            .then(data=>console.log(data))
+    },[])
+
+
     return (
         <div>
             <div className="row">
                 <div className="col-lg-9 bg-secondary text-white">
                     <h1>Users</h1>
-                    
                 </div>
                 <div className="col-lg-3 bg-success">
                     <div className="pt-2">
                         <h6>Users Added: </h6>
                         <h6>Total Salary: </h6>
                     </div>
+
                 </div>
             </div>
         </div>
