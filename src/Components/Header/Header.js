096a5efcbb84c0cab09ec400a53d8247a1f96535
Masud8851 <masud1971.cse@gmail.com>
2021-09-25 23:28:05 +0600

    Assignment 8

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,8 +5,9 @@ const Header = () => {
     return (
         <div>
             <div className="header">
-                <h1>World's Top 10 Prime Minister</h1>
-                <p>A prime minister is the head of the cabinet and the leader of the ministers in the executive branch of government, often in a parliamentary or semi-presidential system. ... In many systems, the prime minister selects and may dismiss other members of the cabinet, and allocates posts to members within the government.</p>
+                <h1>World's Prime Minister</h1>
+                <p>A prime minister is the head of the cabinet and the leader of the ministers in the executive branch of government, often in a parliamentary or semi-presidential system. In many systems, the prime minister selects and may dismiss other members of the cabinet, and allocates posts to members within the government.</p>
+                <h4>Let's see the top 10 from there!</h4>
             </div>
         </div>
     );
