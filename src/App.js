6a5a260887b04b8f7eaba3e2032cd45b6a913115
Masud8851 <masud1971.cse@gmail.com>
2021-09-26 01:48:15 +0600

    Assignment 8

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import Header from './Components/Header/Header';
-import PrimeMinister from './Components/PrimeMinisters/PrimeMinisters'
+import Main from './Components/Main/Main';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      <PrimeMinister></PrimeMinister>
+      <Main></Main>
     </div>
   );
 }
