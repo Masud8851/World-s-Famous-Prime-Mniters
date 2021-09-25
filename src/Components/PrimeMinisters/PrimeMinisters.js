3e6ca35b161856bdefa0cfabc7dabc622cbd3435
Masud8851 <masud1971.cse@gmail.com>
2021-09-26 00:48:00 +0600

    Assignment 8

--- src/Components/PrimeMinisters/PrimeMinisters.js
+++ src/Components/PrimeMinisters/PrimeMinisters.js
@@ -3,7 +3,18 @@ import React from 'react';
 const PrimeMinisters = () => {
     return (
         <div>
-            <h1>Hello</h1>
+            <div className="row">
+                <div className="col-lg-9 bg-secondary text-white">
+                    <h1>Users</h1>
+                    
+                </div>
+                <div className="col-lg-3 bg-success">
+                    <div className="pt-2">
+                        <h6>Users Added: </h6>
+                        <h6>Total Salary: </h6>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
