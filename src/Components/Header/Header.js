d3e9c13f553d9a05105201b1c9e53f6447dc3502
Masud8851 <masud1971.cse@gmail.com>
2021-09-25 23:19:59 +0600

    Assignment 8

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../Header/Header.css'
+
+const Header = () => {
+    return (
+        <div>
+            <div className="header">
+
+            </div>
+        </div>
+    );
+};
+
+export default Header;
