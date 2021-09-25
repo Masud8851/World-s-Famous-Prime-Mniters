263e5c7ca88acd86c619d094ec13ed5194f76521
Masud8851 <masud1971.cse@gmail.com>
2021-09-25 23:48:36 +0600

    Assignment 8

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header/Header';
+import PrimeMinister from './Components/PrimeMinisters/PrimeMinisters'
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <PrimeMinister></PrimeMinister>
     </div>
   );
 }
